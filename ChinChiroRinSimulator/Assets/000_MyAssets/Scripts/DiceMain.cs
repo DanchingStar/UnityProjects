cd91d868cd3d9d6bffa4e2eb858227e7631c315c
Danching Star <danchingstar@gmail.com>
2024-07-10 16:35:45 +0900

    Warning除去

--- ChinChiroRinSimulator/Assets/000_MyAssets/Scripts/DiceMain.cs
+++ ChinChiroRinSimulator/Assets/000_MyAssets/Scripts/DiceMain.cs
@@ -131,7 +131,7 @@ public class DiceMain : MonoBehaviour
         else
         {
             rb.isKinematic = true;
-            rb.velocity = Vector3.zero;
+            //rb.velocity = Vector3.zero;
         }
     }
 
